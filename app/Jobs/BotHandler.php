5c6a93aaa300af656bdb9812b42af8b46cb01149
SaahilChawande <chawandesaahil600@gmail.com>
2018-03-25 23:31:17 +0530

    Handling a message event

--- app/Jobs/BotHandler.php
+++ app/Jobs/BotHandler.php
@@ -13,22 +13,37 @@ class BotHandler implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    //the messaging instance sent to our bothandler
+    protected $messaging;
+
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Messaging $messaging)
     {
+        $this->messaging = $messaging;
     }
 
     /**
      * Execute the job.
      *
-     * @return void
+     * @param Messaging $messaging
      */
     public function handle()
     {
-        //
+        if ($this->messaging->getType() == "message")   {
+            $bot = new Bot($this->messaging);
+            $custom = $bot->extractDataFromMessage();
+            // a request for a new question
+            if ($custom["type"] == Trivia::$NEW_QUESTION)   {
+                $bot->reply(Trivia::getNew($custom['user_id']));
+            }   else if ($custom["type"] == Trivia::$ANSWER)    {
+                $bot->reply(Trivia::checkAnswer($custom["data"]["answer"], $custom['user_id']));
+            }   else    {
+                $bot->reply("I don't understand. Try \"new\" for a new question");
+            }
+        }
     }
 }
